a278f6ad8f6b1cc23caaa4a5bd7322d100c682ce
学渣 <119509966+xuezha-w@users.noreply.github.com>
2025-04-02 20:59:53 +0800

    docs: add documentation

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/docs/",
+  title: "ZBC服务器",
+  description: "笨蛋学渣什么都没有留下",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '查看服规', link: '/Rules.md' }
+    ],
+
+    sidebar: [
+      {
+        text: '指南',
+        items: [
+          { text: '服规', link: '/Rules.md' },
+          { text: '赞助', link: '/Sponsor.md' },
+          { text: '封神榜', link: '/BanList.md' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/zhaQvQ/ZBC-Documentation' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "ZBC服务器"
+  tagline: 笨蛋学渣什么都没有留下
+  actions:
+    - theme: brand
+      text: 查看服规
+      link: /Rules.md
+    - theme: alt
+      text: 赞助我们
+      link: /Sponsor.md
+    - theme: alt
+      text: 封神榜
+      link: /BanList.md
+
+features:   
+  - title: 玩法多样，乐趣无穷  
+    details: "生存、建筑、养老、生电、PVE，应有尽有，总有一种玩法适合你！"  
+  - title: 纯净生电，稳定长久  
+    details: "专注生电玩法，长期稳定运营，打造高质量服务器，拒绝跑路，让你的每一份努力都有回报！"  
+  - title: 加入我们，一起创造  
+    details: "想成为肝帝的一员？快来加入 ZBC！访问: https://b23.tv/2VkXaYd"
+
+---
+
