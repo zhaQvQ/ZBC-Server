093e042ff020e3608e0fe68de7d9d3c1489b6e6f
学渣 <119509966+zhaQvQ@users.noreply.github.com>
2025-04-02 21:31:27 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/zhaQvQ/ZBC-Documentation' }
+      { icon: 'github', link: 'https://github.com/zhaQvQ/ZBC-Server' }
     ]
   }
 })
